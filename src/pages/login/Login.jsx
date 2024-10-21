6a1b5ccbcc46c957a3573714ad5e2d1cfb4c0981
Valentina Jimenez <valentinajt@hotmail.com>
2024-10-21 12:00:42 -0500

    cambios de juan traidos correctamente, y el logout solucionado

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -23,11 +23,7 @@ const Login = () => {
       };
       UserDAO.createUser(newUser).then((response) => {
         if (response.success) {
-<<<<<<< HEAD
-          navigate("/Quiz");
-=======
           navigate("/Welcome");
->>>>>>> a7a224e1cd80a31b2d1316152540836da3a46cbc
         } else {
           console.error("Error al crear el usuario:", response.error);
         }
@@ -59,15 +55,7 @@ const Login = () => {
         </>
       ) : (
         <>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <p className="welcome-text">Bienvenido! Cada Gota Cuenta</p>
-          <button onClick={handleLogin}>Iniciar sesión</button>
-        </>
-      )}
-=======
-=======
->>>>>>> a7a224e1cd80a31b2d1316152540836da3a46cbc
+
           <p className="welcome-text">Bienvenido! Cada Gota Cuenta</p>
           <button className= "button-login" onClick={handleLogin}>Iniciar sesión</button>
         </>
@@ -75,10 +63,7 @@ const Login = () => {
       <div className="waves">
         <img src="/waves.png" alt="Waves img" className="waves-img"/>
       </div>
-<<<<<<< HEAD
->>>>>>> 10a87a6958a733f9ffeb93636f2ace8b8caab133
-=======
->>>>>>> a7a224e1cd80a31b2d1316152540836da3a46cbc
+
     </div>
   );
 };
