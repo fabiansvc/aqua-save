2455d3b64efa6a7985a09d0a7324a19ba734eb0a
JuanEPN <juan.pereira.neira@correounivalle.edu.co>
2024-10-28 17:48:18 -0500

    Cambios en Water.jsx

--- src/pages/waterpollution/Water.jsx
+++ src/pages/waterpollution/Water.jsx
@@ -2,20 +2,14 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/rapier";
 import Beachtown from "./models-3d/Beachtown";
-import Boardmodel from "./models-3d/Boardmodel";
 import Staging from "../staging/Staging";
 import Controls from "../../controls/Controls";
 import Lights from "../../lights/Lights";
 import WelcomeText from "./WelcomeText";
 import Oceanwaves from "./models-3d/Oceanwaves";
 
-const Water = () => {
-  const boardPositions = [
-    { position: [-6, 1, 6.8] },
-    { position: [5.3, 2, 6.8] },
-    { position: [-5, 2, 4.8] },
-    { position: [2.8, 2, 6.8] },
-  ];
+const Water = () => { 
+  
 
   return (
     <Canvas>
