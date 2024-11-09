6186cc62a417ae5f43e0a2efbfd086faaa24614d
DavidRS2354 <david.rodriguez.sanchez@correounivalle.edu.co>
2024-11-09 15:28:03 -0500

    3D Text and style

--- src/pages/watershortages/WaterShortText.jsx
+++ src/pages/watershortages/WaterShortText.jsx
@@ -1,15 +1,23 @@
 import { Html } from "@react-three/drei";
+import { Text3D, Text } from "@react-three/drei";
+
 
 const WaterShortText = () => {
         return(
             <>
-            <Html transform  distanceFactor={3} position={[0.3,3,-2]}>
-                <div className="Title">
-                <h1>ESCASEZ DEL AGUA</h1>
-                
-                </div>
-                
-            </Html>
+            <Text3D
+                position={[-2.3,3,-2]}
+                font={"/fonts/DRAGON HUNTER_Regular.json"}
+                bevelEnabled
+                bevelSize={0.02}
+                bevelThickness={0.1}
+                height={0.5}
+                lineHeight={0.5}
+                letterSpacing={0.05}
+                size={0.3}>
+                    ESCASEZ DEL AGUA
+                    <meshStandardMaterial color="#1681e6" />
+            </Text3D>
             <Html transform  distanceFactor={2.3} position={[-2.1,2.7,-2]}>
                 <h2>PREVENCION DE LA ESCASEZ DEL AGUA</h2>
             </Html>
