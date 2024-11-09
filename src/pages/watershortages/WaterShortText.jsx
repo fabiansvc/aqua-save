39c065066d0505a3e01cfbe6ca9407fa5ce05a6c
DavidRS2354 <david.rodriguez.sanchez@correounivalle.edu.co>
2024-11-09 14:53:52 -0500

    Sub titles of for the videos and some css

--- src/pages/watershortages/WaterShortText.jsx
+++ src/pages/watershortages/WaterShortText.jsx
@@ -2,9 +2,21 @@ import { Html } from "@react-three/drei";
 
 const WaterShortText = () => {
         return(
+            <>
             <Html transform  distanceFactor={3} position={[0.3,3,-2]}>
+                <div className="Title">
                 <h1>ESCASEZ DEL AGUA</h1>
+                
+                </div>
+                
             </Html>
+            <Html transform  distanceFactor={2.3} position={[-2.1,2.7,-2]}>
+                <h2>PREVENCION DE LA ESCASEZ DEL AGUA</h2>
+            </Html>
+            <Html transform  distanceFactor={2.3} position={[2.1,2.7,-2]}>
+                <h2>CAUSAS Y CONSECUENCIAS</h2>
+            </Html>
+            </>
         );
 };
 
