7fb724c3fb0625f50d7da02c78510bbb05088310
JuanEPN <juan.pereira.neira@correounivalle.edu.co>
2024-11-15 17:35:35 -0500

    New appareance of waterpollution pages

--- src/pages/waterpollution/Implication.jsx
+++ src/pages/waterpollution/Implication.jsx
@@ -1,14 +1,65 @@
-import React from 'react'; 
-
+import React from "react";
+import "./Treatment.css";
+import { Canvas } from "@react-three/fiber";
+import Coral from "./models-3d/Coral";
+import { OrbitControls } from "@react-three/drei";
+import { useNavigate } from "react-router-dom";
 
 const Implication = () => {
-  return (
-    <div>
-      <h1>Hola!</h1>
-      <p>Pagina en construccion</p>
-    </div>
-  );
-}
+  const navigate = useNavigate();
 
+  const handleCLickA = () => {
+    navigate("/Sitemap");
+  };
+
+  const handleClickB = () => {
+    navigate("/waterpollution");
+  };
+
+  return (
+    <main className="-container">
+      <button className="buttonBackWater" onClick={handleClickB}>Volver a la página anterior</button>
+      <button className="buttonBackSite" onClick={handleCLickA}> Volver a la página principal</button>
+
+      <h1 className="-title">Implicación de personas y empresas</h1>
+      <section className="implication-introduction">
+        <h2 className="implication-introduction-title">¿Que responsabilidad tienen?</h2>
+        <p className="implication-description">
+
+          Tanto las personas como las empresas tienen una responsabilidad fundamental en la protección del agua como recurso vital. Los individuos pueden contribuir mediante el uso consciente y eficiente del agua, evitando el desperdicio y reduciendo la contaminación al no arrojar productos químicos o basura a fuentes hídricas. Por su parte, las empresas tienen la obligación de adoptar prácticas sostenibles, como implementar sistemas de tratamiento de aguas residuales, minimizar el uso de productos químicos contaminantes y garantizar que sus procesos de producción respeten las normativas ambientales. Además, ambas partes pueden promover la educación y sensibilización sobre la importancia de preservar este recurso para garantizar su disponibilidad a largo plazo.
+
+        </p>
+      </section>
+
+      <section className="Scene1">
+        <Canvas>
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[5, 5, 5]} />
+          <Coral />
+        </Canvas>
+      </section>
+
+      <h2 className="Scene-title">Representación</h2>
+
+      <section className="Scene2">
+        <Canvas>
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[5, 5, 5]} />
+          <Coral />
+        </Canvas>
+      </section>
+      <section className="implication-explanation">
+        <h2 className="implication-title-explanation">
+          ¿Como pueden ayudar a su preservación?
+        </h2>
+        <p className="implication-description">
+          Las personas y las empresas pueden contribuir significativamente a la preservación del agua adoptando prácticas responsables. A nivel individual, es fundamental ahorrar agua en actividades cotidianas, como cerrar el grifo mientras no se utiliza, reparar fugas y optar por tecnologías eficientes en el hogar. También pueden participar en iniciativas de limpieza de cuerpos de agua y fomentar la educación ambiental en sus comunidades. Las empresas, por su parte, pueden implementar sistemas de reciclaje de agua, reducir el consumo en sus procesos productivos y tratar adecuadamente sus aguas residuales antes de verterlas en el medio ambiente. La colaboración entre ambos sectores es clave para garantizar la sostenibilidad de este recurso esencial.
+        </p>
+      </section>
+    </main >
+  );
+};
 
 export default Implication;
--- src/pages/waterpollution/Watercare.jsx
+++ src/pages/waterpollution/Watercare.jsx
@@ -1,14 +1,63 @@
-import React from 'react';
-
+import React from "react";
+import "./Treatment.css";
+import { Canvas } from "@react-three/fiber";
+import Coral from "./models-3d/Coral";
+import { OrbitControls } from "@react-three/drei";
+import { useNavigate } from "react-router-dom";
 
 const Watercare = () => {
+  const navigate = useNavigate();
+
+  const handleCLickA = () => {
+    navigate("/Sitemap");
+  };
+
+  const handleClickB = () => {
+    navigate("/waterpollution");
+  };
 
   return (
-    <div>
-      <h1>Hola!</h1>
-      <p>Pagina en construccion</p>
-    </div>
+    <main className="watercare-container">
+      <button className="buttonBackWater" onClick={handleClickB}>Volver a la página anterior</button>
+      <button className="buttonBackSite" onClick={handleCLickA}> Volver a la página principal</button>
+
+      <h1 className="watercare-title">Cuidado del agua</h1>
+      <section className="watercare-introduction">
+        <h2 className="watercare-introduction-title">¿Que es?</h2>
+        <p className="watercare-description">
+          El cuidado del agua es el conjunto de prácticas y acciones que buscan preservar este recurso vital para garantizar su disponibilidad y calidad para las generaciones presentes y futuras. Implica evitar su desperdicio, prevenir la contaminación de fuentes hídricas, y promover el uso responsable y sostenible en actividades diarias como el consumo doméstico, la agricultura y la industria. Cada gota cuenta en la protección de este recurso esencial para la vida y el equilibrio de los ecosistemas.
+        </p>
+      </section>
+
+      <section className="Scene1">
+        <Canvas>
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[5, 5, 5]} />
+          <Coral />
+        </Canvas>
+      </section>
+
+      <h2 className="Scene-title">Representación</h2>
+
+      <section className="Scene2">
+        <Canvas>
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[5, 5, 5]} />
+          <Coral />
+        </Canvas>
+      </section>
+      <section className="watercare-explanation">
+        <h2 className="watercare-title-explanation">
+          ¿Como se cuida el agua?
+        </h2>
+        <p className="watercare-description">
+          Cuidar el agua es fundamental para asegurar su disponibilidad a largo plazo. Algunas de las acciones clave para proteger este recurso incluyen reducir el consumo innecesario, utilizando técnicas de ahorro como cerrar el grifo mientras no se esté usando, reparar fugas en las instalaciones y optar por electrodomésticos eficientes en el uso de agua. Además, es importante evitar la contaminación de ríos, lagos y océanos, no arrojando productos químicos ni desechos al agua. Practicar el reciclaje de aguas grises y promover el uso de tecnologías sostenibles en la agricultura y la industria también son pasos cruciales. Cada pequeña acción cuenta para conservar el agua y proteger el medio ambiente.
+        </p>
+      </section>
+    </main >
   );
-}
+};
 
 export default Watercare;
